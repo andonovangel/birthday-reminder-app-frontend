db7ba2e2303a82428e15e08d2095bc357431dd37
Angel Andonov <andonovangel1@gmail.com>
2023-10-26 19:35:26 +0200

    added validation on login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -26,6 +26,7 @@ import { TokenInterceptorService } from './interceptor/token-interceptor.service
     HttpClientModule,
     NgbModule,
     AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [
     AuthService,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { BirthdayService } from '../birthday/birthday.service';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,10 +11,46 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   loginUserData: any = {}
+  public formGroup!: FormGroup
+  public submitted: boolean = false
 
-  constructor(private auth: AuthService, private router: Router, private birthdayService: BirthdayService) {}
+  constructor(
+    private auth: AuthService, 
+    private router: Router,
+    private birthdayService: BirthdayService
+  ) {}
 
   ngOnInit(): void {
+    this.formGroup = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=!]).*$/),
+        Validators.minLength(10),
+        Validators.maxLength(64)
+      ])
+    })
+  }
+
+  onSubmit() {
+    if (this.formGroup.invalid) {
+      this.submitted = true
+    } else {
+      console.log(this.formGroup.value)
+      // this.birthdayService.createBirthday(this.formGroup.value).subscribe({
+      //   next: res => {
+      //     console.log(res)
+      //     this.router.navigate(['/birthdays'])
+      //   },
+      //   error: err => {
+      //     console.log(err)
+      //   }
+      // })
+      // this.formGroup.reset()
+    }
   }
 
   loginUser() {
