87f8b2c0cc392c1017f97f6c59d2a69930ca7c8f
Angel Andonov <andonovangel1@gmail.com>
2023-10-29 22:25:15 +0100

    added confirmation email and password on register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -12,6 +12,8 @@ export class RegisterComponent implements OnInit{
   public formGroup! : FormGroup
   public emailError: string | undefined
   public nameError: string | undefined
+  public confirmationEmailError: string | undefined
+  public confirmationPasswordError: string | undefined
   public submitted: boolean = false;
 
   constructor(private auth: AuthService, private router: Router) {}
@@ -26,40 +28,54 @@ export class RegisterComponent implements OnInit{
         Validators.required,
         Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
       ]),
+      confirmationEmail: new FormControl('', [
+        Validators.required,
+      ]),
       password: new FormControl('', [
         Validators.required,
         Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=!]).*$/),
         Validators.minLength(8),
         Validators.maxLength(20)
+      ]),
+      confirmationPassword: new FormControl('', [
+        Validators.required,
       ])
     })
   }
 
   onSubmit() {
-    if (this.formGroup.invalid) {
-      this.submitted = true
-    } else {
-      console.log(this.formGroup.value)
-      this.auth.registerUser(this.formGroup.value).subscribe({
-        next: res => {
-          console.log(res)
-          this.router.navigate(['/welcome'])
-        },
-        error: err => {
-          console.log(err)
-          
-          if (err.error.errors['email']) {
-            this.emailError = err.error.errors['email']
-            this.formGroup.controls['email'].setErrors({'incorrect': true})
-          }
+    this.submitted = true
+    this.auth.registerUser(this.formGroup.value).subscribe({
+      next: res => {
+        console.log(res)
+        this.router.navigate(['/welcome'])
+      },
+      error: err => {
+        console.log(err)
+        
+        if (err.error.errors['email']) {
+          this.emailError = err.error.errors['email']
+          this.formGroup.controls['email'].setErrors({'incorrect': true})
+        }
 
-          if (err.error.errors['name']) {
-            this.nameError = err.error.errors['name']
-  
-            this.formGroup.controls['name'].setErrors({'incorrect': true})
-          }
-        },
-      })
-    }
+        if (err.error.errors['name']) {
+          this.nameError = err.error.errors['name']
+
+          this.formGroup.controls['name'].setErrors({'incorrect': true})
+        }
+
+        if (err.error.errors['confirmationEmail']) {
+          this.confirmationEmailError = err.error.errors['confirmationEmail']
+
+          this.formGroup.controls['confirmationEmail'].setErrors({'incorrect': true})
+        }
+
+        if (err.error.errors['confirmationPassword']) {
+          this.confirmationPasswordError = err.error.errors['confirmationPassword']
+
+          this.formGroup.controls['confirmationPassword'].setErrors({'incorrect': true})
+        }
+      },
+    })
   }
 }
