53123c564ed53c6e439295d00a3d9756d0fb11db
Angel Andonov <andonovangel1@gmail.com>
2024-01-18 17:37:57 +0100

    moved child group panel component code for animation and style to pared sidenav component

--- src/app/sidebar/child-componenets/groups-panel/groups-panel.component.ts
+++ src/app/sidebar/child-componenets/groups-panel/groups-panel.component.ts
@@ -1,25 +1,12 @@
-import { trigger, transition, style, animate } from '@angular/animations';
 import { Component, Input } from '@angular/core';
 import { IGroup } from 'src/app/group/group';
 
 @Component({
   selector: 'app-groups-panel',
   templateUrl: './groups-panel.component.html',
-  styleUrls: ['./groups-panel.component.scss'],
-  animations: [
-      trigger('options', [
-          transition(':enter', [
-            style({ transform: 'translateX(-100%)' }),
-            animate('500ms ease-out', style({ transform: 'translateX(0)' })),
-          ]),
-          transition(':leave', [
-            animate('500ms ease-out', style({ transform: 'translateX(-100%)' })),
-          ]),
-      ]),
-  ]
+  styleUrls: ['./groups-panel.component.scss']
 })
 export class GroupsPanelComponent{
   @Input() groups?: IGroup[]
-  @Input() isGroupsPanelExpanded?: boolean
 
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -2,11 +2,23 @@ import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angu
 import { IGroup } from '../group/group';
 import { Subscription } from 'rxjs';
 import { GroupService } from '../group/group.service';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
+  animations: [
+      trigger('panelAnimation', [
+          transition(':enter', [
+            style({ transform: 'translateX(-100%)' }),
+            animate('500ms ease-out', style({ transform: 'translateX(0)' })),
+          ]),
+          transition(':leave', [
+            animate('500ms ease-out', style({ transform: 'translateX(-100%)' })),
+          ]),
+      ]),
+  ]
 })
 export class SidebarComponent implements OnInit, OnDestroy {
   public isGroupsPanelExpanded: boolean = false
