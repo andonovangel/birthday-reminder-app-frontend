d6f713f821148a1e3baa6c8cb88ed313988a37be
Angel Andonov <andonovangel1@gmail.com>
2024-02-07 12:12:39 +0100

    styled profile detail

--- src/app/user-profile/profile-detail/profile-detail.component.ts
+++ src/app/user-profile/profile-detail/profile-detail.component.ts
@@ -3,15 +3,24 @@ import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/auth/auth.service';
 import { IUser } from '../user';
 import { UserProfileService } from '../user-profile.service';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-profile-detail',
   templateUrl: './profile-detail.component.html',
-  styleUrls: ['./profile-detail.component.scss']
+  styleUrls: ['./profile-detail.component.scss'],
+  animations: [
+    trigger('options', [
+      transition(':enter', [style({ opacity: 0 }), animate('200ms', style({ opacity: 1 }))]),
+      transition(':leave', [animate('200ms', style({ opacity: 0 }))]),
+    ]),
+  ]
 })
 export class ProfileDetailComponent implements OnInit, OnDestroy {
   public user?: IUser
   private getUserSub?: Subscription
+
+  public isOptionVisible: boolean = false
 
   constructor (private auth: AuthService, private userService: UserProfileService) {}
 
@@ -27,5 +36,9 @@ export class ProfileDetailComponent implements OnInit, OnDestroy {
 
   logoutUser() {
     this.auth.logoutUser()
+  }
+
+  toggleOptionsVisibility() {
+    this.isOptionVisible = !this.isOptionVisible
   }
 }
